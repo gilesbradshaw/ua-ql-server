b9d6efa4792600d5bda6ed2b17ab451755a03215
Giles <giles.bradshaw@sigyl.com>
2017-01-11 01:28:27 +0000

    ..

---
+++ certificates/config.js
@@ -0,0 +1,14 @@
+"use strict";
+// ---------------------------------------------------------------------------------------------------------------------
+module.exports =  {
+    commonName:         "NodeOPCUA-COMMONNAME",
+    organization:       "NodeOPCUA",
+    organizationUnit:   "Unit",
+    locality:           "Paris",
+    state:              "IDF",
+    country:            "FR", // Two letters
+    validity:           365 * 15, // 15 years
+
+    keySize:            2028 // default private key size
+};
+
